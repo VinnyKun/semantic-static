08f5e73ea577bd44b2a069796a64687d4f5f40e1
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-11-18 22:47:01 +0800

    login validate works

--- semantic/javascript/validate.js
+++ semantic/javascript/validate.js
@@ -1,64 +1,65 @@
-$('.ui.form')
-  .form({
-    fields: {
-      firstName: {
-        identifier: 'firstName',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your first name'
-          }
-        ]
-      },
-      lastName: {
-        identifier: 'lastName',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your last name'
-          }
-        ]
-      },
-      email: {
-        identifier: 'email',
-        rules: [
-          {
-            type   : 'email',
-            prompt : 'Please key in your email'
-          }
-        ]
-      },
-      username: {
-        identifier: 'username',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a username'
-          }
-        ]
-      },
-      password: {
-        identifier: 'password',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter a password'
-          },
-          {
-            type   : 'minLength[6]',
-            prompt : 'Your password must be at least {ruleValue} characters'
-          }
-        ]
-      },
-      terms: {
-        identifier: 'terms',
-        rules: [
-          {
-            type   : 'checked',
-            prompt : 'You must agree to the terms and conditions'
-          }
-        ]
-      }
-    }
-  })
-;
+$(document).ready(function(){
+    $('.ui.form')
+    .form({
+        fields: {
+        firstName: {
+            identifier: 'firstName',
+            rules: [
+            {
+                type   : 'empty',
+                prompt : 'Please enter your first name'
+            }
+            ]
+        },
+        lastName: {
+            identifier: 'lastName',
+            rules: [
+            {
+                type   : 'empty',
+                prompt : 'Please enter your last name'
+            }
+            ]
+        },
+        email: {
+            identifier: 'email',
+            rules: [
+            {
+                type   : 'email',
+                prompt : 'Please key in your email'
+            }
+            ]
+        },
+        username: {
+            identifier: 'username',
+            rules: [
+            {
+                type   : 'empty',
+                prompt : 'Please enter a username'
+            }
+            ]
+        },
+        password: {
+            identifier: 'password',
+            rules: [    
+            {
+                type   : 'empty',
+                prompt : 'Please enter a password'
+            },
+            {
+                type   : 'minLength[6]',
+                prompt : 'Your password must be at least {ruleValue} characters'
+            }
+            ]
+        },
+        terms: {
+            identifier: 'terms',
+            rules: [
+            {
+                type   : 'checked',
+                prompt : 'You must agree to the terms and conditions'
+            }
+            ]
+        }
+        }
+    });
+}); 
