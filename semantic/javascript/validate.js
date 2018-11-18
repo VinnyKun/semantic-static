21828b68cb8471951938a252a918c77f63fd61bf
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-11-18 21:59:25 +0800

    validate and tab change not working

---
+++ semantic/javascript/validate.js
@@ -0,0 +1,64 @@
+$('.ui.form')
+  .form({
+    fields: {
+      firstName: {
+        identifier: 'firstName',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your first name'
+          }
+        ]
+      },
+      lastName: {
+        identifier: 'lastName',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your last name'
+          }
+        ]
+      },
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type   : 'email',
+            prompt : 'Please key in your email'
+          }
+        ]
+      },
+      username: {
+        identifier: 'username',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a username'
+          }
+        ]
+      },
+      password: {
+        identifier: 'password',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a password'
+          },
+          {
+            type   : 'minLength[6]',
+            prompt : 'Your password must be at least {ruleValue} characters'
+          }
+        ]
+      },
+      terms: {
+        identifier: 'terms',
+        rules: [
+          {
+            type   : 'checked',
+            prompt : 'You must agree to the terms and conditions'
+          }
+        ]
+      }
+    }
+  })
+;
